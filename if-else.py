22c83492acf864787063ac1189e9f52281454259
Adarsh Gour <adarshgour082@gamil.com>
2025-11-20 11:27:50 +0530

    added my if-else file

---
+++ if-else.py
@@ -0,0 +1,53 @@
+#if.....
+
+#x=int(input("enter any value:"))
+#if(x/2==0):
+ #   print("given value is odd")
+  #  print("thanks for visit")
+
+#if-else....
+
+#x=int(input("enter any value"))
+#if(x>0):
+   # print("given value is positive")
+#else:
+   # print("not a even negative")
+   # print("thanks for checking")
+
+#if-elif...
+#n=int(input("enter any value"))
+#if n>0:
+   #print(f'given value{n}is positive' )
+#elif n==0 :
+ #print (f'given value{n}is zero' )
+#elif n<0 :
+ #print (f'given value{n}is negative')
+
+#####if-elif-else
+#x=int(input("enter any value"))
+#if x>0 and x<=10:
+   # print(f'given value{x}lies in b/w 0-10')
+#elif x>10 and x<=20:
+    #print(f'given value{x}lies in b/w 11-20')
+#elif x>20 and x<=30:
+   
+    #print(f'given value{x}lies in b/w 20-30')
+#else:
+   # print(f'given value{x}not valid for check condition')
+    
+#x=int(inpu("enter any value"))
+#(year%4==0 and year%100!=0)or year%400==0:
+    
+#n=int(input("enter how many natural no.you want:"))
+#i=1
+#while(i<=n):
+    # if i<=(n-1):
+          # print (i,end='+')
+#else:
+    # print(i)
+     #i=i+1
+
+     
+    
+    
+    
