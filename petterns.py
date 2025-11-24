614844af3e8d0584a6ad21fdc8b115f4fb89de45
Adarsh Gour <adarshgour082@gamil.com>
2025-11-24 12:41:47 +0530

    added petterns.py file

---
+++ petterns.py
@@ -0,0 +1,41 @@
+'''n=5
+i=1
+while True:
+    j=1
+    while j<=n:
+        print(j,end=' ')
+        j+=1
+    print()
+    i+=1
+    if not (i<=n):
+        break'''
+
+'''square 12345##########
+
+n=int(input("enter any no.to print patterns"))
+for i in range(n):
+    for j in range(1,n+1):
+        print(j,end=" ")
+    print()'''
+
+
+'''Left side angle#########
+n=int(input("enter any no.to print patterns"))
+for i in range(n+1):
+    for j in range(1,i+1):
+
+        print(j,end=" ")
+    print()'''
+
+
+'''12345 angle######
+n=int(input("enter any no,to pettern"))
+number=1
+for i in range(1,n):
+    for j in range(1,i+1):
+        print (number,end="")
+        number=number+1
+    print()'''
+
+
+
