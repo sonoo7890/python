f8a00e3fb6fe1f4128a824a6ee5d0e0fb0ea29a5
Adarsh Gour <adarshgour082@gamil.com>
2025-12-04 16:58:08 +0530

    added my new_3.py file

---
+++ new_3.py
@@ -0,0 +1,11 @@
+
+import keyword
+
+print (keyword.kwlist)
+print (len(keyword.kwlist))
+
+print (keyword.softkwlist)
+print (len(keyword.softkwlist))
+
+print(keyword.kwlist+keyword.softkwlist)
+print(len(keyword.kwlist+keyword.softkwlist))
